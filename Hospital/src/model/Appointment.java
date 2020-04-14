6d14b22eb3bbcfff91a61942781167a285bd992f
Kaumadee Nisanga <kaumadeegunarathna@gmail.com>
2020-04-14 19:43:48 +0530

    change appointment

--- Hospital/src/model/Appointment.java
+++ Hospital/src/model/Appointment.java
@@ -1,29 +1,5 @@
 package model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+public class Appointment {
 
-
-public class Item
-{
-
-	private Connection connect()  
-	{
-		Connection con = null; 
-	
-	 
-	  try   
-	  {   
-		  Class.forName("com.mysql.jdbc.Driver"); 
-		  //Provide the correct details: DBServer/DBName, username, password    
-		  con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");   
-	 }
-	  catch (Exception e)  
-	  {
-		  e.printStackTrace();
-	  }
-	 
-	  return con; 
-	}
-	 
-	 
+}
