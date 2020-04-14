07141cb770762f1d08095d1a688f9258c9156bc2
Kaumadee Nisanga <kaumadeegunarathna@gmail.com>
2020-04-14 19:41:33 +0530

    First commit.

--- Hospital/src/model/Appointment.java
+++ Hospital/src/model/Appointment.java
@@ -1,5 +1,29 @@
 package model;
 
-public class Appointment {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-}
+
+public class Item
+{
+
+	private Connection connect()  
+	{
+		Connection con = null; 
+	
+	 
+	  try   
+	  {   
+		  Class.forName("com.mysql.jdbc.Driver"); 
+		  //Provide the correct details: DBServer/DBName, username, password    
+		  con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");   
+	 }
+	  catch (Exception e)  
+	  {
+		  e.printStackTrace();
+	  }
+	 
+	  return con; 
+	}
+	 
+	 
