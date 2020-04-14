d928497a3d422922f63951e6e5cb5c59fcfab318
Sapthaka Godage <53206450+Sapthaka2346@users.noreply.github.com>
2020-04-14 15:46:04 -0700

    Connect

--- Hospital/src/com/HospitalService.java
+++ Hospital/src/com/HospitalService.java
@@ -11,7 +11,7 @@ import org.jsoup.*;
 import org.jsoup.parser.*;
 import org.jsoup.nodes.Document;
 
-@Path("/Hospital")
+@Path("/HospitalSer")
 public class HospitalService {
 	Hospital itemObj = new Hospital();
 
